9238e49c22593ebbe54d4a894be9961ad03f17fe
Brandon Hancock <Brandon9598@gmail.com>
2018-03-23 22:16:09 -0400

    Finished CRUD

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Mercer_Craigslist.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mercer_Craigslist.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public CategoriesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET: Categories
+        public ActionResult Catagories()
+        {
+            return View();
+        }
+    }
+}
--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -47,5 +47,44 @@ namespace Mercer_Craigslist.Controllers
 
             return View(item);
         }
+
+        [HttpPost]
+        public ActionResult Update(Item item)
+        {
+            var itemInDb = _dbContext.Items.SingleOrDefault(i => i.Id == item.Id);
+
+            if (itemInDb == null)
+                return HttpNotFound();
+
+            itemInDb.Title = item.Title;
+            itemInDb.Description = item.Description;
+            itemInDb.Cost = item.Cost;
+            itemInDb.OwnerEmail = item.OwnerEmail;
+            itemInDb.Category = item.Category;
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var item = _dbContext.Items.SingleOrDefault(i => i.Id == id);
+
+            if (item == null)
+                return HttpNotFound();
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult DoDelete(int id)
+        {
+            var item = _dbContext.Items.SingleOrDefault(i => i.Id == id);
+            if (item == null)
+                return HttpNotFound();
+            _dbContext.Items.Remove(item);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
